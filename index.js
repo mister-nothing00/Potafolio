350b77fc85e5c3af0cd16e953085db43bf88a18d
mister-nothing00 <102729428+mister-nothing00@users.noreply.github.com>
2022-06-11 17:24:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+$("#story").hide();
+$(".down").click(()=>{
+    $("#story").slideToggle(1500);
+})
+$("#skill").hide();
+$(".arrow").mouseover(()=>{
+    $("#skill").slideToggle(2000);
+})
+
+
